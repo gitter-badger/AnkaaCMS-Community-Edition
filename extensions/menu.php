6412dc05c453be10d5d405ddbd42fdaff0cc678f
Dempsey van Wissen <dvanwissen@dienstkoning.nl>
2015-06-02 15:23:48 +0200

    Menu + page + newsletter enhancements
    - Menu enhanced with 'current' class dynamic
    - Newsletter module added

--- extensions/menu.php
+++ extensions/menu.php
@@ -12,18 +12,33 @@ class menu extends extender{
     public $output;
 
     public function __construct(){
+        parent::__construct();
         $top[0] = array(
                         'href'  => '/Home',
                         'title' => 'Homepage',
                         'name'  => 'Home',
-                        'class' => 'current'
                         );
         $top[1] = array(
                         'href'  => '/About',
                         'title' => 'About Us',
                         'name'  => 'About',
-                        'class' => ''
                         );
+        foreach($top as $row=>$item){
+            if($this->getCurrentItem($item['name'])){
+                $top[$row]['class'] = 'current';
+            } else {
+                $top[$row]['class'] = '';
+            }
+        }
         $this->output['top'] = $top;
+    }
+
+    private function getCurrentItem($item){
+        $aRequest = explode('/', $this->request);
+        if($item == $aRequest[0]){
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 }
---
+++ modules/newsletter/index.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * index short summary.
+ *
+ * index description.
+ *
+ * @version 1.0
+ * @author Dempsey
+ */
+class newsletter extends extender{
+    private $data = array();
+    public $template;
+    public $output;
+
+    public function __construct($data=''){
+        parent::__construct();
+        if(!$data == ''){
+            $this->data = json_decode($data);
+        }
+        $this->getArticle();
+    }
+    
+    public function getArticle(){
+        foreach($this->data as $key=>$data){
+            switch($key){
+                case "id":
+                    $this->db->queryRow('SELECT * FROM articles WHERE id = :id', array(':id'=>$data));
+                    $article = $this->db->return;
+                    if(count($article) > 0){
+                        $this->template = $this->getTemplate($article['template']);
+                        $this->output['template'] = $this->template;
+                        $this->output['article'] = $article;
+                    }
+                    break;
+            }
+        }
+    }
+    
+    public function getTemplate($id){
+        $this->db->queryRow('SELECT file FROM article_templates WHERE id = :id', array(':id'=>$id));
+        $file = $this->cwd.'/modules/article/'.$this->db->return['file'];
+        return $file;
+    }
+    
+    public function setArticle(){
+        $data = json_encode(array('id'=>1));
+        echo $data;
+    }
+    
+}
