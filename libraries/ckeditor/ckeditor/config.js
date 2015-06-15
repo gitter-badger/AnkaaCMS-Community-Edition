e324bb426c6455686edcaca5f3f412b7ce82ed38
Dempsey van Wissen <dvanwissen@dienstkoning.nl>
2015-06-15 17:09:15 +0200

    ckeditor added for newsletter
    - composer.json edited with ckeditor support
    - added ckeditor

---
+++ libraries/ckeditor/ckeditor/config.js
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+ CKEDITOR.editorConfig = function( config )
+{
+ config.toolbarGroups: [
+                {"name":"basicstyles","groups":["basicstyles"]},
+                {"name":"links","groups":["links"]},
+                {"name":"paragraph","groups":["list","blocks"]},
+                {"name":"document","groups":["mode"]},
+                {"name":"insert","groups":["insert"]},
+                {"name":"styles","groups":["styles"]}
+            ];
+}
