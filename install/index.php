e793fe87f82011a3836703caea326e110a84112a
root <root@dev.dienstkoning.nl>
2015-06-24 07:01:56 +0000

    Modified .htaccess, install/index.php and widgets/header_imageSlider/index.php Deleted  webdata folder from repository as this should be outside the webroot and automaticly be filled by installer when done

--- install/index.php
+++ install/index.php
@@ -1,55 +1,66 @@
 <?php
-
-if(!isset($_POST['send'])){
-    echo '
-        Please fill in your database settings to import the database file.
-        <form method="POST" action="" name="database">
-            <input type="text" placeholder="User" name="user" />
-            <input type="text" placeholder="Pass" name="pass" />
-            <input type="text" placeholder="Database" name="dbase" />
-            <input type="text" placeholder="Host" name="host" />
-            <input type="submit" value="Install" name="send" />
-        </form>
-        ';
-} elseif($_POST['send']){
-    $user  = $_POST['user'];
-    $pass  = $_POST['pass'];
-    $host  = $_POST['host'];
-    $dbase = $_POST['dbase'];
-    
-    try{
-        $dsn = 'mysql:host='.$host.';dbname='.$dbase;
-        $db = new PDO($dsn, $user, $pass);
-        try{
-            $sql = file_get_contents('./database.sql');
-            if( $iDB = $db->exec($sql) ){
-                echo 'Installed succesfully';
+ini_set('error_reporting', 1);
+function settings($section, $setting){
+        $sCWD = $_SERVER['DOCUMENT_ROOT'];
+        $aCWD = explode(DIRECTORY_SEPARATOR, $sCWD);
+        for($i=0;$i<count($aCWD);$i++){
+            $path = implode($aCWD, DIRECTORY_SEPARATOR);
+            if(file_exists($path.DIRECTORY_SEPARATOR.'webdata')){
+                $webdata = $path.DIRECTORY_SEPARATOR.'webdata';
+                break;
+            } else { 
+                array_pop($aCWD);
             }
-        } catch(PDOException $e){
+        }
+        try{
+            $iniPath = $webdata.DIRECTORY_SEPARATOR.'config.ini';
+            if(file_exists($iniPath)){
+                $parsedIni = parse_ini_file($iniPath, true);
+                $parsedIni;
+                if(isset($parsedIni[$section][$setting])){
+                    return $parsedIni[$section][$setting];
+                } else {
+                    return '';
+                }
+                
+            } else {
+                throw new Exception(_('No config file present'));
+            }
+        }
+        catch(Exception $e){
             echo $e->getMessage();
         }
-    } catch(PDOException $e){
-        try{
-            $dsn = 'mysql:host='.$host.';';
-            $db = new PDO($dsn, $user, $pass);
-            $prep = $db->prepare("CREATE DATABASE :dbase;
-                            GRANT ALL ON :dbase.* TO :user@:host;
-                            FLUSH PRIVILEGES;");
-            $prep->execute(array(':dbase'=>$dbase, ':user'=>$user, ':host'=>$host));
-            } catch(PDOException $e){
-                echo $e->getMessage();
-                echo '
-                    Please fill in your database settings to import the database file.
-                    <form method="POST" action="" name="database">
-                        <input type="text" placeholder="User" name="user" />
-                        <input type="text" placeholder="Pass" name="pass" />
-                        <input type="text" placeholder="Database" name="dbase" />
-                        <input type="text" placeholder="Host" name="host" />
-                        <input type="submit" value="Install" name="send" />
-                    </form>
-                    ';
-            }
     }
+  
+$sql = file_get_contents('./database.sql');
+
+if(!settings('database', 'hostname')){
+    echo _('Please fill your config.ini file first!');
+}
+
+$dsn = 'mysql:host='.settings('database','hostname').';port='.settings('database','port').';';
+try{
+    $db = new PDO($dsn, settings('database','username'), settings('database','password'));
+    try{
+        $db->exec('USE '.settings('database', 'database'));
+        try{
+            $db->exec($sql);
+            echo _('Done');
+        } catch (PDOException $e) {
+            echo _('Database content could not be imported');
+        }
+    } catch (PDOException $e) {
+        try{
+            $db->exec('CREATE DATABASE '.settings('database', 'database').'; USE '.settings('database', 'database'));
+            $db->exec($SQL);
+            echo _('Done');
+        } catch(PDOException $e){
+            echo _('Database does not exist and could not create one.');
+        }
+    }
+} catch(PDOException $e){
+    echo $e->getMessage();
+    //header('Location: /install/');
 }
 
 ?>
